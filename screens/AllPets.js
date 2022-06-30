7debe560c87ac8e0da27211692669f41179b90fe
bukky300 <bukkybrown11@gmail.com>
2022-06-30 09:47:42 +0100

    added caching with asyncStorage

--- screens/AllPets.js
+++ screens/AllPets.js
@@ -1,14 +1,17 @@
 import { useContext, useEffect, useState } from "react";
 import { View, StyleSheet } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import CatList from "../components/CatOutput/CatList";
 import ErrorOverlay from "../components/ui/ErrorOverlay";
 import { CatsContext } from "../store/cats-context";
 import { fetchCats } from "../util/http";
+import { FavoritesContext } from "../store/favorites-context";
 
 function AllPets() {
   const [error, setError] = useState();
   const catsCtx = useContext(CatsContext);
+  const favoritesCtx = useContext(FavoritesContext);
 
   useEffect(() => {
     async function getCats() {
@@ -22,8 +25,18 @@ function AllPets() {
     getCats();
   }, []);
 
+  useEffect(() => {
+    async function fetchCachedfavorites() {
+      const storedFavIds = await AsyncStorage.getItem("favorites");
+      if (storedFavIds) {
+        const storedFavIdsArr = storedFavIds.split(",");
+        favoritesCtx.setFavorites(storedFavIdsArr);
+      }
+    }
+    fetchCachedfavorites();
+  }, []);
+
   if (error) {
-    console.log(error);
     return <ErrorOverlay message={error} />;
   }
 
